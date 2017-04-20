0f786effed832a7beb63f59eccef94135ce57959
PawelPluciennik <pawciopl62@gmail.com>
2017-04-20 17:32:54 +0200

    First change

---
+++ Energy Meter Program/EnergyCalculatov20.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Energy_Meter_Program
+{
+    public class EnergyCalculator
+    {
+
+        public double InitilalState;
+        public double FinalState;
+        public double PricekWh;
+        public double Wear;
+        public double FinalPrice;
+
+        public string Message;
+
+
+        public double Calculate()
+        {
+            CalculateDifference();
+            GetEnergyCost();
+
+            return Wear;
+        }
+        private void CalculateDifference()
+        {
+            Wear = FinalState - InitilalState;
+
+        }
+
+
+        private void GetEnergyCost()
+        {
+            FinalPrice = Wear * PricekWh;
+        }
+
+
+        public EnergyCalculator(double initilalstate, double finalstate, double pricekwh)
+        {
+            InitilalState = initilalstate;
+            FinalState = finalstate;
+            PricekWh = pricekwh;
+        }
+    }
+}
---
+++ Energy Meter Program/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Energy_Meter_Program
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            double InitialStane = Double.Parse(tBoxInitialState.Text);
+            double FinalStane = Double.Parse(tBoxFinalState.Text);
+            double PricekWh = Double.Parse(tBoxPricekWh.Text);
+            InitializeComponent();
+        }
+
+        private void btnCalculateEverything_Click(object sender, RoutedEventArgs e)
+        {
+            EnergyCalculator zuzycie = new EnergyCalculator(tBoxInitialState.Text,tBoxFinalState.Text,tBoxPricekWh.Text);
+           // zuzycie.
+         
+        }
+    }
+}
